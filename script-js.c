8b14e1cf96a5a3fbf1be6a64a22cfb580bf4f2ec
Pratyusha Gupta <tyushagupta@gmail.com>
2015-10-23 19:55:05 +0000

    Textbox - can't type anything in

--- script-js.c
+++ script-js.c
@@ -64,16 +64,6 @@ query (void)
                              "<Image>/Filters/Javascript-Fu");
 }
 
-
-
-
-static void
-hello (GtkWidget *widget,
-       gpointer data)
-{
-  g_print ("Hellow World \n");
-}
-
 static void
 run (const gchar      *name,
      gint              nparams,
@@ -97,29 +87,50 @@ run (const gchar      *name,
   run_mode = param[0].data.d_int32;
 
   if (run_mode != GIMP_RUN_NONINTERACTIVE) {
-   /*  g_message("This is the new output!\n"); */
 
     gimp_ui_init ("jsfu", FALSE);
 
     GtkWidget *window;
     GtkWidget *button;
     GtkTextBuffer *buffer;
+    GtkWidget *textbox;
+    GtkWidget *vbox;
+    GtkWidget *scrolled_window;
 
     window = gimp_dialog_new ("JS-Fu Console", "jsfu", NULL,
                            0, gimp_standard_help_func, "script-js",
                            GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
                            GTK_STOCK_OK, GTK_RESPONSE_OK,
                            NULL);
+    
+    vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
+    gtk_container_set_border_width (GTK_CONTAINER (vbox), 12);
+    gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (window))),
+                        vbox, TRUE, TRUE, 0);
+    gtk_widget_show (vbox);
 
-  /* gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-  button = gtk_button_new_with_label ("Hellow World");
-  g_signal_connect (button, "clicked", G_CALLBACK (hello), NULL);
-  gtk_container_add (GTK_CONTAINER (window), button);
-  gtk_widget_show (button);
-  gtk_widget_show (window);
-  gtk_main(); */
+    scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), 
+                                     GTK_POLICY_AUTOMATIC,
+                                     GTK_POLICY_ALWAYS);
+    gtk_box_pack_start (GTK_BOX (vbox), scrolled_window, TRUE, TRUE, 0);
+    gtk_widget_show (scrolled_window);
+
+    buffer = gtk_text_buffer_new (NULL);
+    textbox = gtk_text_view_new_with_buffer (buffer);
+    g_object_unref (buffer);
+
+    gtk_text_view_set_editable (GTK_TEXT_VIEW (textbox), FALSE);
+    gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (textbox), GTK_WRAP_WORD);
+    gtk_text_view_set_left_margin (GTK_TEXT_VIEW (textbox), 6);
+    gtk_text_view_set_right_margin (GTK_TEXT_VIEW (textbox), 6);
+    gtk_widget_set_size_request (textbox, 400, 480);
+
+
+    gtk_container_add (GTK_CONTAINER (scrolled_window), textbox);
 
     gtk_widget_show (window);
+    gtk_widget_show (textbox);
     int run = (gimp_dialog_run (GIMP_DIALOG (window)) == GTK_RESPONSE_OK);
     gtk_widget_destroy (window);
   }
