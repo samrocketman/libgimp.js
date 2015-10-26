3a9d8800495da1c3ee50eaa988c6b43753f436eb
Pratyusha Gupta <tyushagupta@gmail.com>
2015-10-26 19:51:00 +0000

    Console working!

--- script-js.c
+++ script-js.c
@@ -14,6 +14,8 @@ static void run   (const gchar      *name,
                    const GimpParam  *param,
                    gint             *nreturn_vals,
                    GimpParam       **return_vals);
+static void js_fu_submit (GtkWidget *widget,
+                          GtkTextBuffer *buffer);
 
 GimpPlugInInfo PLUG_IN_INFO =
 {
@@ -86,6 +88,7 @@ run (const gchar      *name,
    * we are in NONINTERACTIVE mode */
   run_mode = param[0].data.d_int32;
 
+  /* Set up the dialog */
   if (run_mode != GIMP_RUN_NONINTERACTIVE) {
 
     gimp_ui_init ("jsfu", FALSE);
@@ -96,39 +99,61 @@ run (const gchar      *name,
     GtkWidget *textbox;
     GtkWidget *vbox;
     GtkWidget *scrolled_window;
+    GtkTextIter iter;
+    const gchar *text = "Welcome to Javascript-Fu Console\nInteractive Javascript Development\n\n\0";
+    const gchar *strong = "strong";
+    const gchar *submit = "Submit...";
 
+    /* Main dialog */
     window = gimp_dialog_new ("JS-Fu Console", "jsfu", NULL,
                            0, gimp_standard_help_func, "script-js",
                            GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
                            GTK_STOCK_OK, GTK_RESPONSE_OK,
                            NULL);
     
+    /* Box to package all of the pieces */
     vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 12);
     gtk_container_set_border_width (GTK_CONTAINER (vbox), 12);
     gtk_box_pack_start (GTK_BOX (gtk_dialog_get_content_area (GTK_DIALOG (window))),
                         vbox, TRUE, TRUE, 0);
+
+    /* Show the box */
     gtk_widget_show (vbox);
 
+    /* Create the scrolled window */
     scrolled_window = gtk_scrolled_window_new (NULL, NULL);
     gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), 
                                      GTK_POLICY_AUTOMATIC,
                                      GTK_POLICY_ALWAYS);
+    
+    /* Pack and show the scrolled window */
     gtk_box_pack_start (GTK_BOX (vbox), scrolled_window, TRUE, TRUE, 0);
     gtk_widget_show (scrolled_window);
 
+    /* Create the text buffer and textbox */
     buffer = gtk_text_buffer_new (NULL);
+    gtk_text_buffer_get_start_iter (buffer, &iter);
+    gtk_text_buffer_insert_with_tags_by_name (buffer, &iter, text, -1, strong, NULL);
     textbox = gtk_text_view_new_with_buffer (buffer);
-    g_object_unref (buffer);
+    
+    /* Create the button */
+    button = gtk_button_new_with_label(submit);
+    gtk_misc_set_padding (GTK_MISC (gtk_bin_get_child (GTK_BIN (button))), 0, 0);
+    gtk_box_pack_start (GTK_BOX (vbox), button, FALSE, TRUE, 0);
+    gtk_widget_show (button);
+    
+    /* Connect the callback signal for when the button is pressed */
+    g_signal_connect (button, "clicked", G_CALLBACK (js_fu_submit), buffer);
 
-    gtk_text_view_set_editable (GTK_TEXT_VIEW (textbox), FALSE);
+    /* Set the properties of the textbox */
+    gtk_text_view_set_editable (GTK_TEXT_VIEW (textbox), TRUE);
     gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (textbox), GTK_WRAP_WORD);
     gtk_text_view_set_left_margin (GTK_TEXT_VIEW (textbox), 6);
     gtk_text_view_set_right_margin (GTK_TEXT_VIEW (textbox), 6);
     gtk_widget_set_size_request (textbox, 400, 480);
 
-
+    /* Add and show the textbox */
     gtk_container_add (GTK_CONTAINER (scrolled_window), textbox);
-
     gtk_widget_show (window);
     gtk_widget_show (textbox);
     int run = (gimp_dialog_run (GIMP_DIALOG (window)) == GTK_RESPONSE_OK);
@@ -136,3 +161,20 @@ run (const gchar      *name,
   }
 }
 
+/* On "Submit" button click */
+static void
+js_fu_submit (GtkWidget *widget, GtkTextBuffer *buffer)
+{
+  GtkTextIter start, end;
+  gchar * text;
+
+  /* Gets the start and stop points for the buffer */
+  gtk_text_buffer_get_start_iter (buffer, &start);
+  gtk_text_buffer_get_end_iter (buffer, &end);
+  text = gtk_text_buffer_get_text (buffer, &start, &end, FALSE);
+
+  printf(text);
+
+  printf("\n");
+  fflush(stdout);
+}
